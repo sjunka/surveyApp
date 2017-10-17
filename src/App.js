b9d801b613abad7285499d94b7eec1bbd5e9bcc2
sergio.junca@gmail.com <sergio.junca@gmail.com>
2017-10-17 10:40:23 -0500

    Inicio proyecto

---
+++ src/App.js
@@ -0,0 +1,23 @@
+//Importar componentes React
+import React, { Component } from 'react';
+
+//Importar Estilos 
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="container">
+          <h1 className="title">Welcome to React</h1>
+        </header>
+        <p className="subtitle">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+//Importar paquetes React
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//Importar Estilos
+import './index.css';
+
+//Importar componentes
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
